6d1352ac25800c0b21cbd570efe70c1015285a07
RamanMilashevich <roma.milashevich@gmail.com>
2025-03-02 13:51:55 +0100

    feat: add mac-address solution

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,10 +14,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+    console.log(n);
+    let parts = n.split('-');
+    console.log(parts);
+    if (parts.length !== 6) {
+      return false;
+    }
+    let hexPattern = /^[0-9A-F]{2}$/;
+  
+    for (let part of parts) {
+      if (!hexPattern.test(part)) {
+        return false;
+      }
+    }
+  
+    return true; 
 }
+
+isMAC48Address('00-1B-63-84-45-E6')
 module.exports = {
   isMAC48Address
 };
