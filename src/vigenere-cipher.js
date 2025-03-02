48f2de78a2791b6332c07d6d8a169254bea9558b
RamanMilashevich <roma.milashevich@gmail.com>
2025-03-02 17:55:12 +0100

    feat: add solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,15 +20,65 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    
+    let result = '';
+    let keyIndex = 0;
+    
+    for (let i = 0; i < message.length; i++) {
+      let char = message[i];
+      
+      if (char >= 'A' && char <= 'Z') {
+        let messageCode = char.charCodeAt(0) - 65;
+        let keyCode = key[keyIndex % key.length].charCodeAt(0) - 65;
+        
+        let encryptedChar = String.fromCharCode(((messageCode + keyCode) % 26) + 65);
+        result += encryptedChar;
+        keyIndex++;
+      } else {
+        result += char;
+      }
+    }
+    
+    return this.direct ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+    
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    
+    let result = '';
+    let keyIndex = 0;
+    
+    for (let i = 0; i < message.length; i++) {
+      let char = message[i];
+      
+      if (char >= 'A' && char <= 'Z') {
+        let messageCode = char.charCodeAt(0) - 65;
+        let keyCode = key[keyIndex % key.length].charCodeAt(0) - 65;
+        
+        let decryptedChar = String.fromCharCode(((messageCode - keyCode + 26) % 26) + 65);
+        result += decryptedChar;
+        keyIndex++;
+      } else {
+        result += char;
+      }
+    }
+    
+    return this.direct ? result : result.split('').reverse().join('');
   }
 }
+
 
 module.exports = {
   VigenereCipheringMachine
